6125fe5112fd6f65f3c26c239fbb8c8c9e69f524
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-03-15 00:33:26 +0900

    map-into-hash-table impl 1

--- map-into-hash-table.lisp
+++ map-into-hash-table.lisp
@@ -5,16 +5,50 @@
 
 (in-package #:y2q-cl-trash/map-into-hash-table)
 
-(defun map-into-hash-table
-    (list &key (key #'identity) (value #'identity) (hash-table (make-hash-table)))
+(defun map-into-hash-table/overwrite (list key-fn value-fn hash-table)
   (loop for i in list
-     as k = (funcall key i)
-     as v = (funcall value i)
-     do (push v (gethash k hash-table)))
-  ;; This code reverses its order.
-  ;; To preserve it, I should make a temporal hash-table, pushes into it, and merge two tables.
-  ;; or -- make a parameter like :if-conflict ?
-  #+ ()
-  (loop for k being the hash-key of table hash-using (hash-value v)
-     do (setf (gethash k hash-table) (nreverse v)))
+     do (setf (gethash (funcall key-fn i) hash-table)
+	      (funcall value-fn i)))
   hash-table)
+
+(defun map-into-hash-table/push (list key-fn value-fn hash-table)
+  ;; This code reverses its order.
+  (loop for i in list
+     do (push (funcall value-fn i)
+	      (gethash (funcall key-fn i) hash-table)))
+  hash-table)
+
+(defun map-into-hash-table/ignore-or-error (list key-fn value-fn hash-table errorp)
+  (loop for i in list
+     as k = (funcall key-fn i)
+     if (nth-value 1 (gethash k hash-table)) ; looks present-p
+     do (when errorp
+	  (error "hash-table ~A already has a value on key ~A"
+		 hash-table k))
+     else
+     do (setf (gethash k hash-table)
+	      (funcall value-fn i)))
+  hash-table)
+
+(defun map-into-hash-table/merge (list key-fn value-fn hash-table merge-fn)
+  (loop for i in list
+     as k = (funcall key-fn i)
+     as v = (funcall value-fn i)
+     do (multiple-value-bind (old-val present-p)
+	    (gethash k hash-table)
+	  (setf (gethash k hash-table)
+		(if present-p
+		    ;; I follow `reduce' convention; (funcall <accumulator> <new value>)
+		    (funcall merge-fn old-val v)
+		    v))))
+  hash-table)
+
+(defun map-into-hash-table (list &key (key #'identity) (value #'identity)
+				   (hash-table (make-hash-table))
+				   (if-exists :overwrite))
+  (case if-exists
+    (:overwrite (map-into-hash-table/overwrite list key value hash-table))
+    (:push (map-into-hash-table/push list key value hash-table))
+    (:ignore (map-into-hash-table/ignore-or-error list key value hash-table nil))
+    (:error (map-into-hash-table/ignore-or-error list key value hash-table t))
+    (otherwise (map-into-hash-table/merge list key value hash-table if-exists))))
