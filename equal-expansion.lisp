2eccc5ebc06bcdfeccee123a291adb1662ab63e6
y2q-actionman <y2q-actionman@users.noreply.github.com>
2019-03-25 17:35:14 +0900

    cleanup

--- equal-expansion.lisp
+++ equal-expansion.lisp
@@ -8,10 +8,11 @@
 
 ;;; rove's `equal*' should written like this.
 (defun equal-expansion (x y)
-  (tree-equal x y
-	      :test (lambda (x y)
-		      ;; These are assumed thanks of `tree-equal'.
-		      (declare (type atom x y))
-		      (or (and (uninterned-symbol-p x)
-			       (uninterned-symbol-p y))
-			  (equal x y)))))
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (flet ((equal-or-uninterned-symbol (x y)
+	   (declare (type atom x y)) ; These are assumed thanks of `tree-equal'.
+	   (or (and (uninterned-symbol-p x)
+		    (uninterned-symbol-p y))
+	       (equal x y))))
+    (declare (dynamic-extent (function equal-or-uninterned-symbol)))
+    (tree-equal x y :test #'equal-or-uninterned-symbol)))
